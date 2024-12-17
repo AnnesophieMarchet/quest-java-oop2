694ab0af6fd45902c1cf9dd64ff09cfab0838c80
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-12-17 19:35:33 +0100

    Revert "Java 07: Héritage"
    This reverts commit aeb86eee9fe7b31802e7709bee96dcf146f71716.

--- Hangar.java
+++
@@ -1,8 +0,0 @@
-public class Hangar {
-    public static void main(String[] args) {
-        Car Mercedes = new Car("Mercedes", 101);
-        Boat PetitNavire = new Boat("Petit Navire", 1234321);
-        System.out.println(Mercedes.doStuff());
-        System.out.printf(PetitNavire.doStuff());
-    }
-}
